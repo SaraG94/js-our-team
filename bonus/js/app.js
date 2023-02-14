07685c5a44246afe79c9ce781219f0ca2a5a10dc
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-14 18:21:01 +0100

    FEAT bonus 1

---
+++ bonus/js/app.js
@@ -0,0 +1,67 @@
+//prendere elemento dal dom
+let idMember = document.getElementById('team-members');
+
+//creare oggetto 
+const teamMember = {
+   fullName : 'Wayne Barnett',
+   businessRole : 'Founder & CEO',
+   pictureProfile : 'wayne-barnett-founder-ceo.jpg'
+}
+
+//creiamo array con indicazioni fornite
+const teamMembers = [
+    teamMember,
+    {  
+        fullName : 'Angela Caroll',
+        businessRole : 'Chief Editor',
+        pictureProfile : 'angela-caroll-chief-editor.jpg'
+    },
+    {  
+        fullName : 'Walter Gordon',
+        businessRole : 'Office Manager',
+        pictureProfile : 'walter-gordon-office-manager.jpg'
+    },
+    {  
+        fullName : 'Angela Lopez',
+        businessRole : 'Social Media Manager',
+        pictureProfile : 'angela-lopez-social-media-manager.jpg'
+    },
+    {  
+        fullName : 'Scott Estrada',
+        businessRole : 'Developer',
+        pictureProfile : 'scott-estrada-developer.jpg'
+    },
+    {  
+        fullName : 'Barbara Ramos',
+        businessRole : 'Graphic Designer',
+        pictureProfile : 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+console.log(teamMembers);
+
+//Stampare su console, per ogni membro del team: nome, ruolo e la stringa della foto (ciclo for in);
+
+for (let key in teamMembers){
+    console.log(key)
+    let member = teamMembers[key];
+    let fullName = member.fullName;
+    let businessRole = member.businessRole;
+    let pictureProfile = member.pictureProfile;
+
+    console.log(fullName,businessRole,pictureProfile);  
+
+    //Stampare le stesse informazioni su DOM sotto forma di stringhe
+    const cardId = `
+    <div class='card'>
+        <div class='card-descrition'>
+            <h2 class="name-member">${fullName}</h2>
+            <p class="role">${businessRole}</p>
+        </div>
+        <div class="card-img">
+            <img src="./img/${pictureProfile}" alt="">
+        </div>
+    </div>
+    `
+    idMember.innerHTML += cardId;
+}
