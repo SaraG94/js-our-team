1dd5971036b32a9b49d60772d7a81125bceba75f
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-14 14:32:38 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,4 @@
+console.log('ciao');
+//creiamo array con indicazioni fornite
+//Stampare su console, per ogni membro del team: nome, ruolo e la stringa della foto (ciclo for in);
+//Stampare le stesse informazioni su DOM sotto forma di stringhe
